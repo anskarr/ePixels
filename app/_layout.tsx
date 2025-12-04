671c811360880288aa313e527dcd65e1e0a7a1d9
Oskar <anskarrcoding@gmail.com>
2025-12-04 16:45:31 +0100

    Push zurück mit neuem Header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
-import MainMenu from "./index";
+import {Stack} from 'expo-router';
 
 export default function RootLayout() {
-  return <MainMenu />;
+  return (
+     <Stack
+           screenOptions={{
+             headerShown: true, // Header anzeigen
+             headerTitle: '',   // Leeren Titel setzen
+             headerBackVisible: false, // Back-Button ausblenden (optional)
+             headerStyle: {
+               backgroundColor: '#fff', // Hintergrundfarbe des Headers
+               height: 60, // Optional: Höhe des Headers anpassen
+             },
+             headerShadowVisible: false, // Optional: Schatten entfernen
+           }}
+         >
+     </Stack>
+  );
 }
