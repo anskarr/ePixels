239185c54c6fc7fd41fee76b9c711bbb121c9104
deChilonio <168724847+oneandonlybasti@users.noreply.github.com>
2026-02-12 15:58:24 +0100

    fixed an incomplete if-statement

--- app/pixelGameGuesser.tsx
+++ app/pixelGameGuesser.tsx
@@ -134,7 +134,6 @@ export default function PixelGuesserScreen() {
 
     const addGuess = () => {
         const newGuess = guess.trim();
-        if(newGuess.equals("Apfel")) => {//play sound};
         if (!newGuess) return // nichts tun wenn leer
         onGuessEnter(prev => [newGuess, ...prev]);
         onGuessText("");
